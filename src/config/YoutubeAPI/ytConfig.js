ec1dbb38e44a2cd5f91607704d39dc0645a703e9
DuongNguyenTino <119667473+DuongNguyenTino@users.noreply.github.com>
2022-12-29 15:57:34 +0700

    Update ytConfig.js

--- src/config/YoutubeAPI/ytConfig.js
+++ src/config/YoutubeAPI/ytConfig.js
@@ -3,7 +3,7 @@ const api_key = process.env.API_KEY;
 
 const getUrl = (endpoint, params) => {
 	const queryString = new URLSearchParams(params);
-	return `${baseUrl}${endpoint}?${queryString}&maxResults=50&regionCode=VN&key=${api_key}`;
+	return `${baseUrl}${endpoint}?${queryString}&maxResults=25&regionCode=VN&key=${api_key}`;
 };
 
 export default { getUrl };
